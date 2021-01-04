8dbbe8ca44d0b369e2a77f2b973cac4d75ea0aa4
suchan-redhat <suchan@redhat.com>
2021-01-04 14:26:09 +0800

    Update ocp311checkcert.sh

--- ocp311checkcert.sh
+++ ocp311checkcert.sh
@@ -46,6 +46,16 @@ for URL in $(oc get route -n openshift describe is |grep tagged | awk '{print $3
     echo 
 done
 
+## LDAPS Cert
+
+echo "------------------------- LDAPS certificate -------------------------"
+APIPOD=$(oc -n kube-system get pod -l 'openshift.io/component=api' --no-headers| awk '{print $1}'|head -n 1)
+for URL in $(oc -n kube-system rsh $APIPOD cat /etc/origin/master/master-config.yaml|grep ldaps| awk -F\/ '{print $3}' |tr -d ' '| sort |uniq ); do
+    echo -n "# $URL URL # "
+    echo | openssl s_client -showcerts -servername $URL  -connect $URL:443 |  show_cert
+    echo 
+done
+
 
 
 
